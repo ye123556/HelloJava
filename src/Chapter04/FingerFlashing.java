50a0d634088eca71bd0583d110ed0ca4d5eda5cc
이유나 <jee1358@naver.com>
2025-01-09 16:47:39 +0900

    2025-1-08 자바 예제 커밋(Chapter 04)

--- src/Chapter04/FingerFlashing.java
+++ src/Chapter04/FingerFlashing.java
@@ -3,71 +3,49 @@ package Chapter04;
 import java.util.Random;
 import java.util.Scanner;
 
-//public class FingerFlashing {
-//    public static void main(String[] args) {
-//        Random rand = new Random();
-//        Scanner sc = new Scanner(System.in);
-//
-   //      //String[] hands = {"가위", "바위", "보"}
-     //   System.out.println("--------------------------");
-     //   System.out.println("0.가위 1.바위 2.보");
-      //  System.out.println("---------------------------");
-       // System.out.println("입력 > ");
+public class FingerFlashing {
+    public static void main(String[] args) {
+        Random rand = new Random();
+        Scanner sc = new Scanner(System.in);
 
-  //      int userHand = sc.nextInt(); // 0 ~ 3
-   //     int computerHand = rand.nextInt(3); // 0~2
+        System.out.println("---------------------------------------------");
+        System.out.println("0.가위  1. 바위  2. 보");
+        System.out.println("---------------------------------------------");
+        System.out.print("입력 > ");
 
-     //   System.out.println("컴퓨터가 낸 것: " );  //hands[valueOfHand]);
+        int userHand = sc.nextInt();
+        int computerHand = rand.nextInt(3); // 0 ~ 2
 
-       // switch(computerHand) {
-         //   case 0:
-           //     System.out.println("가위");
-              //  break;
-            //case 1:
-      //          System.out.println("바위");
-      //          break;
-      //      case 2:
-      //          System.out.println("보");
-        //        break;
-           // //default:
-           // //    throw new RuntimeException("잘못 입력하셨습니다")
-       // }
-        //System.out.print("내가 낸 것: " );
-        //displayHand(computerHand);
+        System.out.print("컴퓨터가 낸 것: " );
+        displayHand(computerHand);
 
-        //if(userHand == computerHand) {
-          //  System.out.println("비겼습니다.");
-  //              }else if((userHand-computerHand  == - 2) || (userHand- computerHand == 1)) {
-    //        System.out.println("당신이 이겼습니다.");
-      //      } else {
-        //    System.out.println("당신이 졌습니다.");
-          //  }
+        System.out.print("내가 낸 것: " );
+        displayHand(userHand);
 
-   //     }
+        displayResult(userHand, computerHand);
+    }
 
-  //  }
-  //  public static void displayHand(int hand) {
-    //    switch(hand) {
-      //      case 0:
-        //        System.out.println("가위");
-          //      break;
-           // case  1:
-             //   System.out.println("바위");
-      //          break;
-      //      case 2:
-        //        System.out.println("보");
-          //      break;
-   //     }
-   // }//void 부분은 int emd gkaus flxjsgownjdigka
+    public static void displayHand(int hand) {
+        switch(hand) {
+            case 0:
+                System.out.println("가위");
+                break;
+            case 1:
+                System.out.println("바위");
+                break;
+            case 2:
+                System.out.println("보");
+                break;
+        }
+    }
 
- //   public static void displayResult(int playerHand, int computerHand) {
-   //     if(playerHand == computerHand) {
-     //       System.out.println("비겼습니다.");
-    //    } else if ((playerHand - computerHand == -2) || (playerHand - computerHand == 1)) {
-      //      System.out.println("당신이 이겼습니다.");
-     //   }else {
-       //     System.out.println("당신이 졌습니다.");
-     //   }
-  //  }
-
-
+    public static void displayResult(int playerHand, int computerHand) {
+        if(playerHand == computerHand) {
+            System.out.println("비겼습니다.");
+        } else if((playerHand - computerHand == -2) || (playerHand - computerHand == 1)) { // 이긴 상태
+            System.out.println("당신이 이겼습니다.");
+        } else {
+            System.out.println("당신이 졌습니다.");
+        }
+    }
+}

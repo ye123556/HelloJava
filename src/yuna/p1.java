ab1b173202c63787038f33b6452b954e2c2e86a4
이유나 <jee1358@naver.com>
2025-01-09 09:05:30 +0900

    2025-1-08 자바 예제 커밋(Chapter 04)

---
+++ src/yuna/p1.java
@@ -0,0 +1,9 @@
+package yuna;
+
+public class p1 {
+    public static void main(String[] args) {
+        String[] names = null;
+        names = new String[]{"신용권", "홍길동", "감자바"};
+        System.out.println(names[0]);
+    }
+}
